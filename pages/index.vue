fb37fffffe2062c2711585541b89cc96b290c6b8
MrT-17 <14235785+MrT-17@users.noreply.github.com>
2025-01-04 19:33:17 +0100

    chore(global config, setup scss & graphql): add global config & setup scss & graphql

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Index.vue</h1>
+    <WeaponBuilder />
+  </div>
+</template>
+
+<script setup lang="ts">
+import WeaponBuilder from '~/pages/weapon-builder.vue'
+</script>
